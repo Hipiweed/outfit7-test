0ac5be5c88fc25d689cd736916b82fd740f617b1
Tilen Vatovec <tilen.vatovec@gmail.com>
2023-10-10 13:30:33 +0200

    formating code

--- apps/fe/src/App.tsx
+++ apps/fe/src/App.tsx
@@ -1,30 +1,36 @@
-import { Table, ActionIcon, Container, AppShell, Burger, Flex, Title, Modal, Button } from '@mantine/core';
+import {
+  Table,
+  ActionIcon,
+  Container,
+  AppShell,
+  Burger,
+  Flex,
+  Title,
+  Modal,
+  Button
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { IconEdit, IconPlus, IconTrash } from '@tabler/icons-react';
 import { EventsApi, EventDto } from './api/api.ts';
 import { useEffect, useState } from 'react';
 import CreateEventForm from './components/createEventForm.tsx';
 
-
-
-
 const App: React.FC = () => {
-
-  const [openedBurger, { toggle:toggleBurger }] = useDisclosure();
+  const [openedBurger, { toggle: toggleBurger }] = useDisclosure();
   const [openedModal, { open: openModal, close: closeModal }] = useDisclosure(false);
-  const [events, setEvents] = useState<EventDto[]>()
+  const [events, setEvents] = useState<EventDto[]>();
   const [selectedEvent, setSelectedEvent] = useState<EventDto | undefined>(undefined);
-
 
   const eventsApi = new EventsApi();
 
   useEffect(() => {
-    eventsApi.eventsControllerGetEvents()
+    eventsApi
+      .eventsControllerGetEvents()
       .then((response) => {
         setEvents(response.data);
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       });
   }, []);
 
@@ -36,11 +42,17 @@ const App: React.FC = () => {
       <Table.Td>{event.priority}</Table.Td>
       <Table.Td>
         <Flex gap="md">
-          <ActionIcon onClick={() => {
-            setSelectedEvent(event) 
-            openModal()
-          }}><IconEdit /></ActionIcon>
-          <ActionIcon><IconTrash /></ActionIcon>
+          <ActionIcon
+            onClick={() => {
+              setSelectedEvent(event);
+              openModal();
+            }}
+          >
+            <IconEdit />
+          </ActionIcon>
+          <ActionIcon>
+            <IconTrash />
+          </ActionIcon>
         </Flex>
       </Table.Td>
     </Table.Tr>
@@ -48,46 +60,53 @@ const App: React.FC = () => {
 
   return (
     <AppShell
-    header={{ height: 60 }}
-    navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !openedBurger } }}
-    padding="md"
-  >
-    <AppShell.Header>
-      <Burger opened={openedBurger} onClick={toggleBurger} hiddenFrom="sm" size="sm" />
-      <Flex justify='center' ><Title order={1}>Event Log App</Title></Flex>
-    </AppShell.Header>
-
-    <AppShell.Navbar p="md"></AppShell.Navbar>
-
-    <AppShell.Main>
-      <Container>
-        <Flex justify={'end'}>
-          <Button my="md" rightSection={<IconPlus />} onClick={() => {
-            setSelectedEvent(undefined) 
-            openModal()
-          }}>New Event</Button>
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !openedBurger } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger opened={openedBurger} onClick={toggleBurger} hiddenFrom="sm" size="sm" />
+        <Flex justify="center">
+          <Title order={1}>Event Log App</Title>
         </Flex>
-        <Table>
-          <Table.Thead>
-            <Table.Tr>
-              <Table.Th>Name</Table.Th>
-              <Table.Th>Description</Table.Th>
-              <Table.Th>Type</Table.Th>
-              <Table.Th>Priority</Table.Th>
-              <Table.Th>Actions</Table.Th>
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>{rows}</Table.Tbody>
-        </Table>
-      </Container>
-    </AppShell.Main>
+      </AppShell.Header>
 
-    <Modal opened={openedModal} onClose={closeModal} title="CreateEvent">
-    <CreateEventForm row={selectedEvent} onClose={closeModal}/>
-    </Modal>
-  </AppShell>
-    
+      <AppShell.Navbar p="md"></AppShell.Navbar>
+
+      <AppShell.Main>
+        <Container>
+          <Flex justify={'end'}>
+            <Button
+              my="md"
+              rightSection={<IconPlus />}
+              onClick={() => {
+                setSelectedEvent(undefined);
+                openModal();
+              }}
+            >
+              New Event
+            </Button>
+          </Flex>
+          <Table>
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th>Name</Table.Th>
+                <Table.Th>Description</Table.Th>
+                <Table.Th>Type</Table.Th>
+                <Table.Th>Priority</Table.Th>
+                <Table.Th>Actions</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{rows}</Table.Tbody>
+          </Table>
+        </Container>
+      </AppShell.Main>
+
+      <Modal opened={openedModal} onClose={closeModal} title="CreateEvent">
+        <CreateEventForm row={selectedEvent} onClose={closeModal} />
+      </Modal>
+    </AppShell>
   );
-}
+};
 
 export default App;
--- apps/fe/src/components/createEventForm.tsx
+++ apps/fe/src/components/createEventForm.tsx
@@ -3,53 +3,49 @@ import { TextInput, Textarea, Select, Button, NumberInput } from '@mantine/core'
 import { CreateEventDto, EventDto, EventType, EventsApi } from '../api';
 
 interface IProps {
-    row?:EventDto
-    onClose: () => void
+  row?: EventDto;
+  onClose: () => void;
 }
 
-
-function SaveEvent(eventData:CreateEventDto, id?:number) {
-    const eventsApi = new EventsApi();
-    if (id)  {
-        eventsApi.eventsControllerUpdateEvent(id, eventData)
-        .catch((error) => {
-            console.log(error)
-        });
-    } else {
-    eventsApi.eventsControllerCreateEvent(eventData)
-    .catch((error) => {
-      console.log(error)
+function SaveEvent(eventData: CreateEventDto, id?: number) {
+  const eventsApi = new EventsApi();
+  if (id) {
+    eventsApi.eventsControllerUpdateEvent(id, eventData).catch((error) => {
+      console.log(error);
     });
-    }
+  } else {
+    eventsApi.eventsControllerCreateEvent(eventData).catch((error) => {
+      console.log(error);
+    });
+  }
 }
-  
-const CreateEventForm: React.FC<IProps> = ({row, onClose}) => {
-    const form = useForm<CreateEventDto>({
+
+const CreateEventForm: React.FC<IProps> = ({ row, onClose }) => {
+  const form = useForm<CreateEventDto>({
     initialValues: {
-        name: row?.name ?? '',
-        description: row?.description ?? '',
-        type: row?.type ?? EventType.App,
-        priority: row?.priority ?? 1,
+      name: row?.name ?? '',
+      description: row?.description ?? '',
+      type: row?.type ?? EventType.App,
+      priority: row?.priority ?? 1
     },
     validate: {
-        name: isNotEmpty("You must enter name"),
-        description: isNotEmpty("You must enter description"),
-        type: (value) => (Object.keys(EventType).includes(value) ? 'Invalid event type': null ) ,
-        priority: (value) => (value < 10 && value < 1 ? 'Priority must be an integer between 1 and 10' : null),
-    },
+      name: isNotEmpty('You must enter name'),
+      description: isNotEmpty('You must enter description'),
+      type: (value) => (Object.keys(EventType).includes(value) ? 'Invalid event type' : null),
+      priority: (value) =>
+        value < 10 && value < 1 ? 'Priority must be an integer between 1 and 10' : null
+    }
+  });
 
-    });
-
-  const eventTypeOptions = Object.values(EventType)
+  const eventTypeOptions = Object.values(EventType);
 
   return (
-    <form onSubmit={form.onSubmit((values: CreateEventDto) => {SaveEvent(values, row?.id), onClose()})}>
-      <TextInput
-        label="Name"
-        id="name"
-        my="sm"
-        {...form.getInputProps('name')}
-      />
+    <form
+      onSubmit={form.onSubmit((values: CreateEventDto) => {
+        SaveEvent(values, row?.id), onClose();
+      })}
+    >
+      <TextInput label="Name" id="name" my="sm" {...form.getInputProps('name')} />
       <Textarea
         label="Description"
         id="description"
@@ -61,10 +57,9 @@ const CreateEventForm: React.FC<IProps> = ({row, onClose}) => {
         label="Type"
         id="type"
         my="sm"
-        data={eventTypeOptions} 
+        data={eventTypeOptions}
         {...form.getInputProps('type')}
-      >
-      </Select>
+      ></Select>
 
       <NumberInput
         my="sm"
@@ -73,12 +68,13 @@ const CreateEventForm: React.FC<IProps> = ({row, onClose}) => {
         min={1}
         max={10}
         {...form.getInputProps('priority')}
-      >
-      </NumberInput>
+      ></NumberInput>
 
-      <Button my="md" type="submit">Submit</Button>
+      <Button my="md" type="submit">
+        Submit
+      </Button>
     </form>
   );
 };
 
-export default CreateEventForm
+export default CreateEventForm;
